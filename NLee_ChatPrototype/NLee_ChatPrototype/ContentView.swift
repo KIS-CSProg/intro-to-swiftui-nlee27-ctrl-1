4e7ccd49e4bf56219e0ed29a707ba614179caebd
Nathan Lee <nlee27@student.kis.or.kr>
2026-04-23 13:03:57 +0900

    Create ChatPrototype Project

---
+++ NLee_ChatPrototype/NLee_ChatPrototype/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NLee_ChatPrototype
+//
+//  Created by Nathan Lee on 4/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NLee_ChatPrototype/NLee_ChatPrototype/NLee_ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NLee_ChatPrototypeApp.swift
+//  NLee_ChatPrototype
+//
+//  Created by Nathan Lee on 4/23/26.
+//
+
+import SwiftUI
+
+@main
+struct NLee_ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
